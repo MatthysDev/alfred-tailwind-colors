cfb2097026bff60814278c9e8df2935754cb9961
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-02-28 14:53:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import alfy from "alfy";
+
+const data = await alfy.fetch("https://jsonplaceholder.typicode.com/posts");
+
+const items = alfy.inputMatches(data, "title").map((element) => ({
+  title: element.title,
+  subtitle: element.body,
+  arg: element.id,
+}));
+
+alfy.output(items);
